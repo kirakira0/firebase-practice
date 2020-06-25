494a6cc102b161eb521eaf8ab7518f7f5b013f1c
kirakira0 <kiratoal0@gmail.com>
2020-06-25 09:51:23 -0700

    report reader processes CSV by row

--- ReportReader.java
+++ ReportReader.java
@@ -7,39 +7,41 @@ import java.io.*;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ReportReader {
 
   private static final String CSV_FILE_PATH = "./data/test.csv";
 
-  public static void readLine(String csvFile) {
-    try {
+  /* 
+   * Reads every row of the CSV into a string array then returns list of row arrays. 
+   */
+  public static List<String[]> readCSV(String csvFile) {
+    List<String[]> allAds = new ArrayList<String[]>();
+    try {    
       FileReader fileReader = new FileReader(new File(csvFile));
       BufferedReader bufferedReader = new BufferedReader(fileReader); // wraps file reader
       String row = "";
-      String[] tempArr;
-      int counter = 0; 
-      while((row = bufferedReader.readLine()) != null && counter < 7) {
-        
-        // tempArr = row.split(DELIMITER);
-        tempArr = row.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", -1);
-
-        for (String str: tempArr) {
-          System.out.println(str); 
+      String[] singleAd = {}; // array stores single row of CSV data (one advertisement)
+      while((row = bufferedReader.readLine()) != null) {
+        singleAd = row.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", -1); // split by CSV column
+        for (int i = 0; i < singleAd.length; i++) {
+          singleAd[i] = singleAd[i].replace("\"", ""); 
         }
+        allAds.add(singleAd); // add ad array to aggregate list 
 
-        System.out.println("\n\n\n"); 
-        counter++; 
       }
       bufferedReader.close();
-    } catch(IOException ioe) {
-      ioe.printStackTrace();
+    } catch(IOException e) {
+      e.printStackTrace();
     }
+    return allAds; 
   }
 
   public static void main(String[] args) {
-    String csvFile = CSV_FILE_PATH;
-    readLine(csvFile);
+    readCSV(CSV_FILE_PATH).forEach(array -> System.out.println(Arrays.toString(array)));
   }
 
 }
